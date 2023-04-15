6992721b2fd92bd79e44f723cd6be837db5e8f55
Christian Göran <christian.goran@protonmail.com>
2023-04-15 09:45:46 +0000

    Initial commit

---
+++ assets/js/questions_array.js
@@ -0,0 +1,251 @@
+const questions = [{
+  questionNumber: 1,
+  questionsText: 'Which cryptocurrency first caught your attention?',
+  answers: [{
+      answerNumber: 1,
+      answerText: 'Bitcoin',
+      points: 3
+    }, 
+    {
+      answerNumber: 2,
+      answerText: 'Ethereum',
+      points: 2
+    },
+    {
+      answerNumber: 3,
+      answerText: 'Other altcoins',
+      points: 1
+    },
+    {
+      answerNumber: 4,
+      answerText: 'None, I am a crypto skeptic',
+      points: 0
+    }
+  ]
+},
+{
+  questionNumber: 2,
+  questionsText: 'What is your preferred investment strategy?',
+  answers: [{
+        answerNumber: 1,
+        answerText: 'Hold long-term and HODL',
+        points: 3
+      },
+      {
+        answerNumber: 2,
+        answerText: 'Trade frequently for short-term gains',
+        points: 2
+      },
+      {
+        answerNumber: 3,
+        answerText: 'Invest in promising altcoins',
+        points: 1
+      },
+      {
+        answerNumber: 4,
+        answerText: 'Avoid investing in cryptocurrency altogether',
+        points: 0
+      }
+    ]
+  },
+  {
+  questionNumber: 3,
+  questionsText: 'How much time do you spend following cryptocurrency news and updates?',
+  answers: [{
+        answerNumber: 1,
+        answerText: "Every day, it's my passion",
+        points: 3
+      },
+      {
+        answerNumber: 2,
+        answerText: 'A few times a week, to stay informed',
+        points: 2
+      },
+      {
+        answerNumber: 3,
+        answerText: 'Occasionally, when something big happens',
+        points: 1
+      },
+      {
+        answerNumber: 4,
+        answerText: 'Never, I have no interest in it',
+        points: 0
+      }
+    ]
+  },
+  {
+  questionNumber: 4,
+  questionsText: 'What is your opinion on the future of cryptocurrency?',
+  answers: [{
+        answerNumber: 1,
+        answerText: 'It will revolutionize the world of finance',
+        points: 3
+      },
+      {
+        answerNumber: 2,
+        answerText: 'It has potential, but may face regulatory hurdles',
+        points: 2
+      },
+      {
+        answerNumber: 3,
+        answerText: 'It will continue to grow, but may not replace traditional finance',
+        points: 1
+      },
+      {
+        answerNumber: 4,
+        answerText: "It's a fad that will eventually fade away",
+        points: 0
+      }
+    ]
+  },
+  {
+  questionNumber: 5,
+  questionsText: 'What do you think is the biggest challenge facing cryptocurrency adoption?',
+  answers: [{
+        answerNumber: 1,
+        answerText: 'Lack of understanding and education',
+        points: 3
+      },
+      {
+        answerNumber: 2,
+        answerText: 'Volatility and market fluctuations',
+        points: 2
+      },
+      {
+        answerNumber: 3,
+        answerText: 'Security and hacking concerns',
+        points: 1
+      },
+      {
+        answerNumber: 4,
+        answerText: 'Lack of government regulation and support',
+        points: 0
+      }
+    ]
+  },
+  {
+  questionNumber: 6,
+  questionsText: 'Which of the following features of cryptocurrency do you value the most?',
+  answers: [{
+        answerNumber: 1,
+        answerText: 'Decentralization and freedom from government control',
+        points: 3
+      },
+      {
+        answerNumber: 2,
+        answerText: 'Smart contract capabilities and decentralized applications',
+        points: 2
+      },
+      {
+        answerNumber: 3,
+        answerText: 'Low transaction fees and fast transaction speeds',
+        points: 1
+      },
+      {
+        answerNumber: 4,
+        answerText: 'None of the above, I do not value any specific feature',
+        points: 0
+      }
+    ]
+  },
+  {
+  questionNumber: 7,
+  questionsText: 'How do you feel about the energy consumption associated with mining cryptocurrency?',
+  answers: [{
+        answerNumber: 1,
+        answerText: "It's a necessary part of the process",
+        points: 3
+      },
+      {
+        answerNumber: 2,
+        answerText: "It's a concern, but can be addressed through sustainable mining practices",
+        points: 2
+      },
+      {
+        answerNumber: 3,
+        answerText: "It's a serious problem that needs to be addressed",
+        points: 1
+      },
+      {
+        answerNumber: 4,
+        answerText: 'I am not aware of the energy consumption associated with mining cryptocurrency',
+        points: 0 
+      }  
+    ]
+  },
+  {
+    questionNumber: 8,
+    questionsText: 'What is your opinion on stablecoins?',
+    answers: [{
+        answerNumber: 1,
+        answerText: "They are a valuable tool for reducing volatility in the cryptocurrency market",
+        points: 3
+      },
+      {
+        answerNumber: 2,
+        answerText: "They have potential, but may face regulatory challenges",
+        points: 2
+      },
+      {
+        answerNumber: 3,
+        answerText: "They are unnecessary and go against the decentralized nature of cryptocurrency",
+        points: 1
+      },
+      {
+        answerNumber: 4,
+        answerText: 'I am not familiar with stablecoins',
+        points: 0 
+      }
+    ]
+  },
+  {
+    questionNumber: 9,
+    questionsText: 'What do you think is the biggest benefit of cryptocurrency?',
+    answers: [{
+        answerNumber: 1,
+        answerText: "Financial freedom and independence from traditional banking systems",
+        points: 3
+      },
+      {
+        answerNumber: 2,
+        answerText: "Increased transparency and accountability in financial transactions",
+        points: 2
+      },
+      {
+        answerNumber: 3,
+        answerText: "Fast and low-cost international transfers",
+        points: 1
+      },
+      {
+        answerNumber: 4,
+        answerText: 'None of the above, I do not see any benefits of cryptocurrency',
+        points: 0 
+      }
+    ]
+  },
+  {
+    questionNumber: 10,
+    questionsText: 'How do you see cryptocurrency evolving in the future?',
+    answers: [{
+        answerNumber: 1,
+        answerText: "It will become the primary form of currency and replace traditional finance",
+        points: 3
+      },
+      {
+        answerNumber: 2,
+        answerText: "It will continue to grow and evolve, but may not replace tradictional finance entirely",
+        points: 2
+      },
+      {
+        answerNumber: 3,
+        answerText: "It will become a niche investment opportunity",
+        points: 1
+      },
+      {
+        answerNumber: 4,
+        answerText: 'It will eventually fade away and be replaced by new technologies',
+        points: 0 
+      }]
+  }
+
+]
