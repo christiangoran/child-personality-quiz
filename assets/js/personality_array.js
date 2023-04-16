231fe0a4ff8486668df24777275a1f0ff85029c3
Christian Göran <christian.goran@protonmail.com>
2023-04-16 13:28:25 +0000

    Add personality & questions array files

--- assets/js/personality_array.js
+++ assets/js/personality_array.js
@@ -1,7 +1,33 @@
 let personalities = [{
 
-        type: 'popular',
-        name: "The Popular Kid",
-        text: "You were social and outgoing as a child, and likely had a large circle of friends. You enjoyed being the center of attention and were interested in fashion, pop culture, and the latest trends."
-        colorCode: ""
-}]
+    type: 'popular',
+    name: "The Popular Kid",
+    text: "You were social and outgoing as a child, and likely had a large circle of friends. You enjoyed being the center of attention and were interested in fashion, pop culture, and the latest trends."
+
+  },
+  {
+    type: 'creative',
+    name: 'The Creative Type',
+    text: "You were imaginative and artistic as a child, and enjoyed expressing yourself through various forms of creativity, such as drawing, writing, or music."
+  },
+  {
+    type: 'collector',
+    name: 'The collector',
+    text: "You had a love for collecting things as a child, whether it was Beanie Babies, Pokemon cards, or other toys. You were likely organized and detail-oriented, and enjoyed the thrill of completing a set."
+  },
+  {
+    type: 'tech',
+    name: 'The tech-savvy kid',
+    text: "You were fascinated by technology and video games as a child, and were likely an early adopter of new gadgets and devices. You enjoyed problem-solving and exploring new worlds through the screen."
+  },
+  {
+    type: 'adventure',
+    name: 'The Adventurous One',
+    text: "You were active and outgoing as a child, and enjoyed trying new things and exploring the outdoors. You had a thirst for adventure and likely participated in sports or other physical activities."
+  },
+  {
+    type: 'fashion',
+    name: 'The Fashionista',
+    text: "You were stylish and trendy as a child, and enjoyed experimenting with fashion and accessories. You had a keen eye for design and aesthetics, and enjoyed expressing yourself through your appearance."
+  },
+]
