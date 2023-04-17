20efad19ab60eab4718485edba1056c2d1e077eb
Christian Göran <christian.goran@protonmail.com>
2023-04-17 06:55:27 +0000

    Add qustion function to populate answer-buttons & progress indication

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -7,12 +7,15 @@ const startButton = document.getElementById('start-button');
 const startArea = document.querySelector('.start-area');
 
 // Consts for questions //
-const questionArea = document.getElementById('question-area');
-const questionParagraph = document.getElementById('question-paragraph');
+const questionArea = document.querySelector('#question-area');
+const questionParagraph = document.querySelector('#question-paragraph');
 
 // Consts for answers //
 const answerArea = document.querySelector('.answer-area');
-const answers = document.getElementsByClassName('answer-button');
+const answers = Array.from(document.querySelectorAll('.answer-button'));
+
+const progress = document.querySelector('#progress');
+
 
 const pieArea = document.getElementById('pie-area');
 
@@ -22,26 +25,43 @@ const resultArea = document.querySelector('#result-area');
 // Name input and gamestart //
 const nameInput = document.getElementById('name-input');
 
+// if username is entered quiz can start when button is pressed //
 if (nameInput.value !== "") {
   answerArea.classList.remove('hide');
   startArea.classList.add('hide');
-  addQuestions(q);
+  addQuestions(0);
 
 } else {
-
+// otherwise user is asked to enter name //
   alert('Please enter your name to start the quiz.')
 }
 
+
+// To understand how to extract values from array: https://www.programiz.com/javascript/examples/extract-value-array//
+function addQuestions(index) {
+
+  let activeQuestion = questions[index];
+  questionParagraph.innerHTML = activeQuestion.questionsText;
+
+  let choices = activeQuestion.answers;
+  for (let i = 0; i < choices.length; i++) {
+    answers[i].innerHTML = choices[i].answerText;
+  }
+
+ let questionNumber = questions[0].questionNumber;
+  progress.innerHTML = `Question ${questionNumber} /10`;
+
 }
 
-function addQuestions() {
+function userAnswer(index) {
+
+
 
 }
 
-function userAnswer() {
-
-}
 
 function handleAnswer() {
+
+}
 
 }
